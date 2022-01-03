a02fde6eda734805d8cf1dacca7bb232a39ff047
Richard Puma <052113@unsaac.edu.pe>
2022-01-03 14:58:04 -0500

    https arreglado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async(category) => {
     //const url ='http://api.giphy.com/v1/gifs/search?api_key=MPdbdJlt8HGPn9eF3XDBF3QfoBDBbsAc&q=Naruto&limit=10';
-    const url = `http://api.giphy.com/v1/gifs/search?api_key=MPdbdJlt8HGPn9eF3XDBF3QfoBDBbsAc&q=${ encodeURI(category)}&limit=10`
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=MPdbdJlt8HGPn9eF3XDBF3QfoBDBbsAc&q=${ encodeURI(category)}&limit=10`
     const resp = await fetch(url);
     const {data} = await resp.json();
 
